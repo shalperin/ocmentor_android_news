6daf9d25d74c4983b5b756d43402d6fb87db876c
Shimi Halperin <sqh@me.com>
2020-06-20 15:11:20 -0400

    constant names

--- MyNews/app/src/main/java/com/example/mynews/Config.kt
+++ MyNews/app/src/main/java/com/example/mynews/Config.kt
@@ -11,17 +11,23 @@ const val appId = BuildConfig.APP_ID
 
 val logLevel = HttpLoggingInterceptor.Level.BODY
 
-const val baseUrl = "https://api.nytimes.com/svc/"
-const val topStoriesPath = "topstories/v2/home.json"
-const val mostPopularPath = "mostpopular/v2/emailed/7.json"
-const val artsPath = "topstories/v2/arts.json"
-const val realEstatePath = "topstories/v2/realestate.json"
-const val technologyPath = "topstories/v2/technology.json"
-const val automobilesPath = "topstories/v2/automobiles.json"
-const val searchPath = "search/v2/articlesearch.json?"
+const val BASE_URL = "https://api.nytimes.com/svc/"
+const val TOP_STORIES_PATH = "topstories/v2/home.json"
+const val MOST_POPULAR_PATH = "mostpopular/v2/emailed/7.json"
+const val ARTS_PATH = "topstories/v2/arts.json"
+const val REAL_ESTATE_PATH = "topstories/v2/realestate.json"
+const val TECHNOLOGY_PATH = "topstories/v2/technology.json"
+const val AUTOMOBILES_PATH = "topstories/v2/automobiles.json"
+const val SEARCH_PATH = "search/v2/articlesearch.json?"
 
-const val staticBasePath = "https://static01.nyt.com/"
+const val STATIC_BASE_PATH = "https://static01.nyt.com/"
 
-const val apiSortOrderRelevance = "relevance"
-const val apiSortOrderNewest = "newest"
-const val apiSortOrderOldest = "oldest"
+const val API_SORT_ORDER_RELEVANCE = "relevance"
+const val API_SORT_ORDER_NEWEST = "newest"
+const val API_SORT_ORDER_OLDEST = "oldest"
+
+const val SEARCH_ALERT_STORAGE_KEY = "searchAlertRequestHistory"
+const val PREFERENCE_FILE = "news_shared_prefs"
+
+const val FOREGROUND = false
+const val BACKGROUND = true
